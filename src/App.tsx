78047421dd94ebb0df79fe2f35c42c44938e38ab
Movacs <kovacsmarcell1213@gmail.com>
2025-08-09 13:45:41 +0200

    MUI navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import AppBackground from './components/background/AppBackground'
+import ButtonAppBar from './components/navbar/NavBar'
+
 
 
 function App() {
   return (
     <div>
       <AppBackground animation={false} />
+      <ButtonAppBar />
 
     </div>
   )
